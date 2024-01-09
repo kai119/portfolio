c74cb1c462e6e37a4c849916923a7642463beb71
Kai Mumford <kai.mumford@roke.co.uk>
2024-01-09 09:10:26 +0000

    Added NextUI components

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const {nextui} = require("@nextui-org/theme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Individual NextUI Components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
